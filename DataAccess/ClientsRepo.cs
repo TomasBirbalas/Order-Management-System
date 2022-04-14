49bb0241f95a6a402631aa9a75755bc3e0a03d56
tomasbir <tomas.birbalas@gmail.com>
2022-04-14 21:34:46 +0300

    Created DataAccess Project to receive the data from JSON

---
+++ DataAccess/ClientsRepo.cs
@@ -0,0 +1,24 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ClientsRepo
+    {
+
+
+        public static async Client Retrieve()
+        {
+            string fileName = @"D:\Programavimo kursai\NET\Order-Management-System\Order Management System\DataAccess\JSONdata\Client.json";
+            using FileStream openStream = File.OpenRead(fileName);
+            Client ClientList = await JsonSerializer.DeserializeAsync<Client>(openStream);
+
+        }
+    }
+}
