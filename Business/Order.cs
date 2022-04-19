d7ecf5f6ac8805313ed198bbf0071bfb71a51e5b
tomasbir <tomas.birbalas@gmail.com>
2022-04-19 21:20:30 +0300

    Removed some fields from Order object

--- Business/Order.cs
+++ Business/Order.cs
@@ -10,14 +10,12 @@ namespace Business
     {
         public int Id { get; set; }
         public DateTime OrderDate { get; set; }
-        public DateTime PaidTill { get; set; }
         public List<OrderProduct> OrderProducts { get; set; }
         public string OrderStatus { get; set; }
-        public Order(int id, DateTime orderDate, DateTime paidTill, List<OrderProduct> orderProducts, string orderStatus)
+        public Order(int id, DateTime orderDate, List<OrderProduct> orderProducts, string orderStatus)
         {
             Id = id;
             OrderDate = orderDate;
-            PaidTill = paidTill;
             OrderProducts = orderProducts;
             OrderStatus = orderStatus;
         }
