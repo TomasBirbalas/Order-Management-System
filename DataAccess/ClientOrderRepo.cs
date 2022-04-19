fb2513f17910439ba12dd6c79ee065a422f17a66
tomasbir <tomas.birbalas@gmail.com>
2022-04-19 15:39:04 +0300

    Created DataAccess Project to get data from JSON

---
+++ DataAccess/ClientOrderRepo.cs
@@ -0,0 +1,21 @@
+﻿using Business;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DataAccess
+{
+    public class ClientOrderRepo
+    {
+        public List<ClientOrder> DeserializeDataFile()
+        {
+            var path = @"..\..\..\..\DataAccess\JSONdata\ProjectData.json";
+            var jsonString = File.ReadAllText(path);
+            List<ClientOrder> clientOrders = JsonSerializer.Deserialize<List<ClientOrder>>(jsonString);
+
+            return clientOrders;
+        }
+    }
+}
