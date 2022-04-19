c0cdd969bd76b4e68ebed452bd20fcd8e3e71d6a
tomasbir <tomas.birbalas@gmail.com>
2022-04-19 13:56:59 +0300

    Created new Project for all services

---
+++ Services/AllOrdersOfClient.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Services
+{
+    public class AllOrdersOfClient
+    {
+        public string Generate()
+        {
+            ClientOrderRepo clientOrderRepo = new ClientOrderRepo();
+            var data = clientOrderRepo.DeserializeDataFile();
+            string result = "";
+            string clientList = "";
+            foreach (var client in data)
+            {
+                client.OrderList.ForEach(order => {
+                    int id = order.Id;
+                    string status = order.OrderStatus;
+
+                    result += $"Uzsakymo id: {id}, statusas: {status} \r\n";
+                });
+                clientList += $"{client.Client.BusinessName} \r\n";
+            }
+            return clientList;
+        }
+    }
+}
