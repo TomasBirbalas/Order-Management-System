675743c246d62eaf448394370d758b04cefb4730
tomasbir <tomas.birbalas@gmail.com>
2022-04-20 13:01:22 +0300

    Created Deserializer Project

---
+++ Deserializer/DeserializeAllData.cs
@@ -0,0 +1,21 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Deserializer
+{
+    public class DeserializeAllData
+    {
+        public List<ClientOrder> DeserializeDataFile()
+        {
+            var path = @"..\..\..\..\DataAccess\JSONdata\ProjectData.json";
+            var jsonString = File.ReadAllText(path);
+            List<ClientOrder> jsonData = JsonSerializer.Deserialize<List<ClientOrder>>(jsonString);
+
+            return jsonData;
+        }
+
+    }
+}
