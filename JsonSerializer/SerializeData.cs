91dbf06401c08ef16171110c8057c13d3b7dd4e2
tomasbir <tomas.birbalas@gmail.com>
2022-04-20 13:03:09 +0300

    Fixed Serializer issue: "No more dublicated items in order"

--- JsonSerializer/SerializeData.cs
+++ JsonSerializer/SerializeData.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace JsonSerializeris
@@ -13,23 +14,6 @@ namespace JsonSerializeris
             Random random = new Random();
             RandomDateTime date = new RandomDateTime();
 
-
-            List<Product> productList = new List<Product>();
-            for (int i = 0; i < 15; i++)
-            {
-                decimal price = random.Next(0, 50) + (decimal)random.NextDouble();
-                price = decimal.Round(price, 2, MidpointRounding.AwayFromZero);
-
-                productList.Add(new Product(100000 + i, $"Preke-{i}", price));
-            }
-
-
-            List<OrderProduct> productsInOrder = new List<OrderProduct>();
-            for (int i = 0; i < random.Next(1, 15); i++)
-            {
-                productsInOrder.Add(new OrderProduct(productList[random.Next(0,15)], random.Next(1,20)));
-            }
-
             List<Address> addressesList = new List<Address>();
             for (int i = 0; i < 10; i++)
             {
@@ -38,15 +22,17 @@ namespace JsonSerializeris
 
             List<ClientOrder> clientsOrders = new List<ClientOrder>();
             List<Client> client = new List<Client>();
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 5; i++)
             {
-                client.Add(new Client(i, " ", 800000000 + i, $"LT{100000000000 + i}", addressesList[random.Next(0, 10)], addressesList[random.Next(0, 10)]));
+                client.Add(new Client(i, $"UAB Company-{i}", 800000000 + i, $"LT{100000000000 + i}", addressesList[random.Next(0, 10)], addressesList[random.Next(0, 10)]));
 
                 List<Order> ordersList = new List<Order>();
-                for (int j = 0; j < 15; j++)
+                for (int j = 0; j < random.Next(1, 15); j++)
                 {
-                    int status = new Random().Next(0, 1);
+                    int status = new Random().Next(0, 2);
                     string orderStatus = status == 1 ? "Completed" : "Pending_payment";
+
+                    List<OrderProduct> productsInOrder = GenerateProductList();
 
                     ordersList.Add(new Order(Convert.ToInt32($"20220{i}000") + j, date.Next(), productsInOrder, orderStatus));
                 }
@@ -55,9 +41,37 @@ namespace JsonSerializeris
 
             var options = new JsonSerializerOptions { WriteIndented = true };
             string jsonString = JsonSerializer.Serialize(clientsOrders, options);
-            var jsonPath = @"..\..\..\..\DataFiles\test.json";
+            var jsonPath = @"..\..\..\..\DataAccess\JSONdata\ProjectData.json";
 
             File.WriteAllText(jsonPath, jsonString);
+        }
+        public List<OrderProduct> GenerateProductList()
+        {
+            Random random = new Random();
+
+            List<Product> productList = new List<Product>();
+            for (int i = 1; i < 15; i++)
+            {
+                decimal price = random.Next(0, 50) + (decimal)random.NextDouble();
+                price = decimal.Round(price, 2, MidpointRounding.AwayFromZero);
+
+                productList.Add(new Product(100000 + i, $"Preke-{i}", price));
+            }
+            
+            List<OrderProduct> productsInOrder = new List<OrderProduct>();
+            for (int i = 0; i < random.Next(1, 10); i++)
+            {
+                bool isProductInOrder = false;
+                Product randomProduct = productList[random.Next(0, 14)];
+
+                isProductInOrder = productsInOrder.Any(x => x.Product.Id == randomProduct.Id);
+
+                if (!isProductInOrder)
+                {
+                    productsInOrder.Add(new OrderProduct(randomProduct, random.Next(1, 20)));
+                }
+            }
+            return productsInOrder;
         }
     }
     class RandomDateTime
