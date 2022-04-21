e96bbcb50d68fb702be81c64a6cacb7b7c625d57
tomasbir <tomas.birbalas@gmail.com>
2022-04-21 09:37:12 +0300

    Added OrderTotalAmount

--- Business/Order.cs
+++ Business/Order.cs
@@ -11,12 +11,14 @@ namespace Business
         public int Id { get; set; }
         public DateTime OrderDate { get; set; }
         public List<OrderProduct> OrderProducts { get; set; }
+        public decimal OrderTotalAmount { get; set; }
         public string OrderStatus { get; set; }
-        public Order(int id, DateTime orderDate, List<OrderProduct> orderProducts, string orderStatus)
+        public Order(int id, DateTime orderDate, List<OrderProduct> orderProducts, decimal orderTotalAmount, string orderStatus)
         {
             Id = id;
             OrderDate = orderDate;
             OrderProducts = orderProducts;
+            OrderTotalAmount = orderTotalAmount;
             OrderStatus = orderStatus;
         }
     }
